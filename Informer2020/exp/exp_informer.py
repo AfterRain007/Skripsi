921dfa1bfc085f378e8efade931ef2fd8e9a7384
AfterRain007 <139695632+AfterRain007@users.noreply.github.com>
2023-07-17 15:51:49 +0700

    Update exp_informer.py

--- Informer2020/exp/exp_informer.py
+++ Informer2020/exp/exp_informer.py
@@ -225,7 +225,7 @@ class Exp_Informer(Exp_Basic):
         np.save(folder_path+'pred.npy', preds)
         np.save(folder_path+'true.npy', trues)
 
-        return
+        return mse
 
     def predict(self, setting, load=False):
         pred_data, pred_loader = self._get_data(flag='pred')
